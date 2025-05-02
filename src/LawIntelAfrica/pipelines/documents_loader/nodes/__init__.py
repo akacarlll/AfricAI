a4479c6a1fcaf6abf5b0cee3493c8a61b8339388
Carl-Dimitri Fogue <147193447+akacarlll@users.noreply.github.com>
2025-05-02 22:37:36 +0200

    🔥 There's the hook to validate columns, so no need for this function

--- src/LawIntelAfrica/pipelines/documents_loader/nodes/__init__.py
+++ src/LawIntelAfrica/pipelines/documents_loader/nodes/__init__.py
@@ -2,12 +2,10 @@ from ._load_documents import load_documents
 from ._add_cols import extract_metadata
 from ._remove_special_characters import remove_characters
 from ._merge_pdfs_texts_data import merge_pdfs_texts_dfs
-from ._validate_col import verify_columns
 
 __all__ = [
     "load_documents",
     "extract_metadata",
     "remove_characters",
     "merge_pdfs_texts_dfs",
-    "verify_columns",
 ]
